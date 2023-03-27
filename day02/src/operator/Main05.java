5ab6874b3865a67f073cd1bdf722280dc575b352
박병훈 <128352331+ByeongHoon-git@users.noreply.github.com>
2023-03-27 14:11:48 +0900

    Create Main05.java

---
+++ day02/src/operator/Main05.java
@@ -0,0 +1,27 @@
+package operator;
+
+public class Main05 {
+
+	public static void main(String[] args) {
+		int num1 = 123;
+		int num2 = 456;
+		
+		// 비교연산자에 대한 결과는 boolean
+		boolean result2 = num1 == num2;
+		System.out.println(result2);
+		
+		// !=  다르다
+		boolean result3 = num1 != num2;
+		System.out.println(result3);
+		
+		// >   초과
+		boolean result4 = num1 > num2;
+		System.out.println(result4);
+		
+		
+		// <=   이하
+		boolean result5 = num1 <= num2;
+		System.out.println(result5);
+	}
+
+}
