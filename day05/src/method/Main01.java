7adb6ec937b84442054cc7c9433a011f54755c3a
박병훈 <128352331+ByeongHoon-git@users.noreply.github.com>
2023-03-27 15:44:39 +0900

    Create Main01.java

---
+++ day05/src/method/Main01.java
@@ -0,0 +1,24 @@
+package method;
+
+public class Main01 {
+
+	public static void main(String[] args) {
+		// 여러번 사용하려구
+		plus();
+		plus();
+		plus();
+		plus();
+		plus();
+		plus();
+	}
+
+	public static void plus() {
+		int x = 100;
+		int y = x + 1;
+		System.out.println(y);
+	}
+	
+	
+	
+	
+}
