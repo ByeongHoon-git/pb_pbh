c94c67c85f366eddf8a6fc43f26565b114fba704
박병훈 <128352331+ByeongHoon-git@users.noreply.github.com>
2023-03-27 16:10:38 +0900

    Create FileArticle.java

---
+++ day06/src/Extends/FileArticle.java
@@ -0,0 +1,23 @@
+package Extends;
+
+public class FileArticle extends Article {
+
+	private String filaName;
+
+	public String getFilaName() {
+		return filaName;
+	}
+
+	public void setFilaName(String filaName) {
+		this.filaName = filaName;
+	}
+	
+	public String toString() {
+		return "자료실[번호 = " + getNum() 
+				+ ", 제목 = " + getTitle()
+				+ ", 첨부파일 = " + getFilaName();
+	}
+	
+	
+	
+}
